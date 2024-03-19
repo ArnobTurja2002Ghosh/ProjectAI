75bc1eba4ed0c5bbb1f227ce2487f31d8872c671
zawadul <57873810+zawadman@users.noreply.github.com>
2024-03-19 13:10:38 -0230

    Add files via upload

---
+++ EnemyCroc.js
@@ -0,0 +1,27 @@
+import { Character } from './Character.js';
+//import { State, PatrolState } from './State.js';
+import * as THREE from 'three';
+
+
+export class EnemyCroc extends Character{
+    constructor(mColor){
+        super(mColor);
+        //this.size = 20;
+        let texture = new THREE.TextureLoader().load('./croc_copy.jpg');
+        let cubeGeo = new THREE.BoxGeometry(10,10,10);//needs implementation
+        let cubeMat = new THREE.MeshStandardMaterial({map: texture});
+
+        let mesh =  new THREE.Mesh(cubeGeo,cubeMat);
+        mesh.position.y = mesh.position.y+1;
+		// Rotate our X value of the mesh so it is facing the +z axis
+		mesh.rotateX(Math.PI/2);
+
+        this.gameObject = new THREE.Group();
+		this.gameObject.add(mesh);
+
+    }
+    update(deltaTime) {
+		super.update(deltaTime,this.gameObject);
+		//this.state.updateState(this, enemy);
+	}
+}
